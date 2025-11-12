d3cb98488093d133da8be5b7df08659d9f7084aa
declanrjb <declanrjb@gmail.com>
2025-11-12 17:32:40 -0500

    update readme

--- web/script.js
+++ web/script.js
@@ -2,4 +2,31 @@ $(function() {
     $('#epstein-search').on('keyup', function(e) {
         console.log(e.currentTarget.value)
     })
+
+    $('#search-exec').on('click', async function(e) {
+        console.log('sending')
+
+        var query = 'https://epstein-search.onrender.com/search?q=Ghislaine&page=0&show=5';
+
+        let response = await fetch(query, {
+            method: 'POST'
+        })
+
+        const r = await response.json();
+        
+        var results = r['records']
+
+        for (var i=0; i<results.length; i++) {
+            $('#results-container').append(
+                `
+                <div class="result">
+                    <p><b>FILENAME</b></p>
+                    <p>HEADER</p>
+                    <p>MATCH_WINDOW</p>
+                </div>
+                `.replace('FILENAME', results[i]['file'])
+            )
+        }
+    })
+
 })
